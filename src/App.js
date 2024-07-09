211f583d887f4740d88c3102682be32fea84a9cc
Ali Nihad <alinihad100@gmail.com>
2024-07-09 17:13:59 +0800

    .

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css'; // Import Ant Design styles
 import Dashboard from './components/Dashboard';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
