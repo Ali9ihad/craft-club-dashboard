aaa34075f7d05dd90f5eea56b9a216646c87ce11
Ali Nihad <alinihad100@gmail.com>
2024-07-25 22:32:36 +0800

    .

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css'; // Import Ant Design styles
 import Dashboard from './components/Dashboard';
-<<<<<<< HEAD
 import 'antd/dist/antd.css';
-=======
->>>>>>> parent of 9a1a8231 (Made the sidebar and it’s wondows functional)
+import { Components } from 'antd/lib/date-picker/generatePicker';
 
 function App() {
   return (
     <div className="App">
       <Dashboard />
+
     </div>
   );
 }
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-// src/components/Dashboard.js
+
 import React from 'react';
 import { Layout, Menu, Breadcrumb } from 'antd';
 import {
